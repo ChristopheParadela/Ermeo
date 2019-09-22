4d4057dafc4fbbd4de57739a0557963e6a7204ca
Christophe <Christophe.Paradela@gmail.com>
2019-09-22 15:50:18 +0200

    feat(template): angular material
    - Anomaly list with angular table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 // Module
+import { AnomalyModule } from './views/anomaly/anomaly.module';
 import { AssetModule } from './views/asset/asset.module';
 import { ErrorModule } from './views/error/error.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AssetModule, ErrorModule],
+  imports: [BrowserModule, AppRoutingModule, AnomalyModule, AssetModule, BrowserAnimationsModule, ErrorModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/views/asset/asset.module.ts
+++ src/app/views/asset/asset.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 // Component
 import { AssetPageComponent } from './page/asset-page.component';
@@ -9,9 +10,19 @@ import { AssetRoutesModule } from './asset.routes';
 
 // Module
 import { HeaderModule } from '../header/header.module';
+import { AnomalyModule } from '../anomaly/anomaly.module';
+import { MatCardModule, MatDividerModule } from '@angular/material';
 
 @NgModule({
   declarations: [AssetPageComponent],
-  imports: [CommonModule, AssetRoutesModule, HeaderModule]
+  imports: [
+    CommonModule,
+    AnomalyModule,
+    AssetRoutesModule,
+    FlexLayoutModule,
+    HeaderModule,
+    MatCardModule,
+    MatDividerModule
+  ]
 })
 export class AssetModule {}
