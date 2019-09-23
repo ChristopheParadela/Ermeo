4a5ee3f4fcd03bd0903ef70ee9106c491fbd5d31
Christophe <Christophe.Paradela@gmail.com>
2019-09-23 14:52:47 +0200

    feat(anomaly): anomaly page and intervention data

---
+++ src/app/views/anomaly/page/anomaly-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anomaly-page',
+  templateUrl: './anomaly-page.component.html',
+  styleUrls: ['./anomaly-page.component.scss']
+})
+export class AnomalyPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/app/views/asset/asset.routes.ts
+++ src/app/views/asset/asset.routes.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AssetPageComponent } from './page/asset-page.component';
+import { AnomalyPageComponent } from '../anomaly/page/anomaly-page.component';
 
 export const assetRoutes: Routes = [
   {
     path: 'asset',
-    children: [{ path: '', redirectTo: '/asset/1', pathMatch: 'full' }, { path: ':id', component: AssetPageComponent }]
+    redirectTo: '/asset/1',
+    pathMatch: 'full'
+  },
+  {
+    path: 'asset/:id',
+    children: [
+      {
+        path: '',
+        component: AssetPageComponent
+      },
+      {
+        path: 'anomaly',
+        children: [{ path: ':id', component: AnomalyPageComponent, pathMatch: 'full' }]
+      }
+    ]
   }
 ];
 
